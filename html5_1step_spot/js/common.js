2839d1aad0cf68c43c116e75afcacff5357d2798
dami <ppppp_ep@naver.com>
2020-03-18 18:19:51 +0900

    0318

--- html5_1step_spot/js/common.js
+++ html5_1step_spot/js/common.js
@@ -1,16 +1,43 @@
 $(function(){
-    $('li').on('mouseenter',function(){        
+    $('nav li').on('mouseenter',function(){        
          $(this).find('div').stop().slideDown(); // 모든 li태그 안에 div 자식요소를 찾음
     });
 
-    $('li').on('mouseleave',function(){        
+    $('nav li').on('mouseleave',function(){        
         $(this).find('div').stop().slideUp();
    });
 
-   $('header p').on('mouseover',function(){
-       $('header p span').animate({
-          
-       },600);
-   });
+   
+//    // 선택자별로 액션
+//    $('nav li').on({
+//        mouseenter : nav1,
+//        mouseleave : nav2,
+//        click : nav
+//     });
+//     function nav1(){
+     
+//     }
+//     function nav2(){
+        
+//     }
+
+
+
+   $('.trigger_menu').on('click',function(){
+       $(this).toggleClass('active');
+        $('header p > span').toggleClass('active');
+       
+    //    if($(this).toggleClass('active')){
+    //     $('header p > span').show().animate({
+    //         top:40, opacity:1
+    //      },);
+    //    }else{
+    //     $('header p > span').show().animate({
+    //         top:60, opacity:0
+    //      },);
+    //    }
+      
+   });   
     
 });
+

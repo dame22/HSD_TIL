5c4a26c72d8e57407372bb09005372869e79f0bb
dami <ppppp_ep@naver.com>
2020-04-09 18:20:45 +0900

    0409

--- pet mate/js/common.js
+++ pet mate/js/common.js
@@ -7,34 +7,51 @@ $(function(){
 
             wCroll = $(this).scrollTop();          
                    
-           if(wCroll > count){//내림               
-                $('header h1').addClass('active');
-                $('header nav').addClass('active');           
-           }else{           
-                $('header h1').removeClass('active');  
-                $('header nav').removeClass('active');                       
-            }
+        //    if(wCroll > count){//내림               
+        //         $('header h1').addClass('active');
+        //         $('header nav').addClass('active');           
+        //    }else{           
+        //         $('header h1').removeClass('active');  
+        //         $('header nav').removeClass('active');                       
+        //     }
 
             count = wCroll;
         });      
         
         $('.main .visual').animate({
             height: 90 + '%'
-        },1500,function(){
+        },2000,function(){
             $('.main .visual img').eq(1).css({
                 zIndex: 0,
                 display: 'block'
-            });                     
-        });
-      
-           
+            });                   
+        });   
        
+        function numberCounter(target_frame, target_number) {
+            this.count = 0; this.diff = 0;
+            this.target_count = parseInt(target_number);
+            this.target_frame = document.getElementById(target_frame);
+            this.timer = null;
+            this.counter();
+            };
         
+            numberCounter.prototype.counter = function() {
+                var self = this;
+                this.diff = this.target_count - this.count;
+            
+                if(this.diff > 0) {
+                    self.count += Math.ceil(this.diff / 5);
+                }
+            
+                this.target_frame.innerHTML = this.count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+            
+                if(this.count < this.target_count) {
+                    this.timer = setTimeout(function() { self.counter(); }, 50);
+                } else {
+                    clearTimeout(this.timer);
+                }
+            };        
+        
+            new numberCounter("counter1", 45678);
+            new numberCounter("counter2", 56789);
 });
-
-
-// window.addEventListener('DOMContentLoade',function(){    ; 
-//     document.documentElement.style.overflowY = 'hidden';
-// })
-
-
